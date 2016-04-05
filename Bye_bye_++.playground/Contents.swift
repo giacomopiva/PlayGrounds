38b784873ed2fe0045db1ca315e350f98d0b3d40
Giacomo Piva <me@giacomopiva.com>
2016-04-05 11:47:43 +0200

    Aggiunto playgroud

---
+++ Bye_bye_++.playground/Contents.swift
@@ -0,0 +1,21 @@
+/**
+ * Bye bye ++ playgroud
+ *
+ * Author: Giacomo Piva - http://www.giacomopiva.com
+ *
+ */
+
+
+var anInt = 0
+
+anInt += 1
+
+anInt.advancedBy(3)
+
+anInt
+
+anInt = 10
+
+anInt -= 1
+
+anInt.advancedBy(-3)
